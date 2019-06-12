2414ed480d0553cc8ad405fe2370738e36d05f35
Taylor Doyle <tcd56@cornell.edu>
2019-06-12 18:40:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 # Add  code here!
 def prime?(num)
-  if num < 0 || num == 0 || num == 1
+  if num <= 1
     return false
   else
-    (2..num-1).to_a_all? do |fact|
-      num % fact != 0
-    end
+    (2..num/2).none? {|n| num % n == 0}
   end
 end
+
